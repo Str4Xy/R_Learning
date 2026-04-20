f80b02470595f7fb077952bdf337a96e3155b82f
Str4Xy <johnp.stuppan@gmail.com>
2026-04-20 11:18:40 +0200

    Initial commit: First R script

---
+++ First_Script.R
@@ -0,0 +1,14 @@
+# Variablen
+name <- "RStudio"
+version <- 4.5
+
+# Vektor
+zahlen <- c(10, 20, 30, 40, 50)
+
+# Einfache Statistik
+mean(zahlen)    # Mittelwert
+sum(zahlen)     # Summe
+length(zahlen)  # Länge
+
+# Ersten Plot
+plot(zahlen)
